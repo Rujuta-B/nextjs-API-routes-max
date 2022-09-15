8f13c0b0915ce08bfde6728a0bf11f8823bf7bda
brujuta <97433576+Rujuta-B@users.noreply.github.com>
2022-09-15 20:18:02 +0530

    connected to mongoDB and connected frontend to API routes

--- nextJs-API-routes/pages/index.js
+++ nextJs-API-routes/pages/index.js
@@ -1,58 +1,34 @@
-import { useRef, useState } from "react";
+import Head from 'next/head';
 
-function HomePage() {
-  const emailInputRef = useRef();
-  const feedbackInputRef = useRef();
-  const [feedbackItems, setFeedbackItems] = useState([]);
+import { getFeaturedEvents } from '../helpers/api-util';
+import EventList from '../components/events/event-list';
+import NewsletterRegistration from '../components/input/newsletter-registration';
 
-  function loadFeedbackHandler() {
-    fetch("/api/feedback")
-      .then((response) => response.json())
-      .then((data) => setFeedbackItems(data.feedback));
-  }
-
-  function submitFormHandler(event) {
-    event.preventDefault();
-
-    console.log("first");
-
-    const enteredEmail = emailInputRef.current.value;
-    const enteredFeedback = feedbackInputRef.current.value;
-
-    const reqBody = { email: enteredEmail, text: enteredFeedback };
-
-    fetch("/api/feedback", {
-      method: "POST",
-      body: JSON.stringify(reqBody),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .then((data) => console.log("data"));
-  }
-
+function HomePage(props) {
   return (
     <div>
-      <h1>The Home Page</h1>
-      <form onSubmit={submitFormHandler}>
-        <div>
-          <label htmlFor="email">Your email</label>
-          <input type="email" id="email" ref={emailInputRef} />
-        </div>
-        <div>
-          <label htmlFor="feedback">Your feedback</label>
-          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
-        </div>
-        <button>Submit Feedabck</button>
-      </form>
-      <hr />
-      <button onClick={loadFeedbackHandler}>load feeedback</button>
-      <ul>
-        {feedbackItems.map((item) => (
-          <li key={item.id}>{item.text}</li>
-        ))}
-      </ul>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
+      <NewsletterRegistration />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
